841f59fb2a4611fec4ae5dcb36642d82987281b3
Aaron Myles Landwehr <snaphat@gmail.com>
2024-01-19 16:50:08 -0500

    Add script to apply all patches

---
+++ _ApplyAllPatches.bat
@@ -0,0 +1,8 @@
+CD /D %~dp0
+powershell .\_GhostNamePatch.ps1
+powershell .\_GolfPausePatch.ps1
+powershell .\_ProjectZeroFileCountPatch.ps1
+powershell .\_RCoasterPausePatch.ps1
+powershell .\_SanFranNamePatch.ps1
+powershell .\_SaturnPausePatch.ps1
+powershell .\_ZodiacPausePatch.ps1
